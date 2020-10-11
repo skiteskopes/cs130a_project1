a77fd639a9aa164ee901b9b9371cf276ece90a42
Bill Zhang <bill_zhang@csilvm-01.cs.ucsb.edu>
2020-10-10 20:10:07 -0700

    ok

---
+++ TST.cpp
@@ -0,0 +1,10 @@
+#include "TST.h"  
+#include "tst_node.h"   
+
+void insert(word) {   
+	if(this->head == NULL) {    
+		head = new TST_node();   
+		head.set_kleft(word);   
+	}   
+	else {   
+		if(
